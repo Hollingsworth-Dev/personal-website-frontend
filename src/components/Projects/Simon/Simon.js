8654301d83be868f9ca09175da72fdebd7b5f4d3
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-09-22 11:47:08 -0500

    fleshed out SIMON page

--- src/components/Projects/Simon/Simon.js
+++ src/components/Projects/Simon/Simon.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import Desktop from '../../../Images/SIMON/SimonDesktop.png';
+import Mobile from '../../../Images/SIMON/SIMON-mobile.png';
+import Title from '../../../Images/SIMON/SIMON-title.png';
+import {
+	AiOutlineLink,
+	AiOutlineGithub,
+	AiOutlineMedium,
+	AiOutlineDesktop,
+} from 'react-icons/ai';
 
 const Simon = () => {
 	return (
 		<div>
 			<h1>SIMON</h1>
 			<main>
-				<div>images of app</div>
 				<div>
-					<h2>Check it out here</h2>
-					<ul>
-						<li>link to deployed app</li>
-						<li>link to github</li>
-						<li>link to blogpost</li>
-					</ul>
+					<p>An application using HTML, CSS, & JavaScript</p>
+					<p>A digital version of the 1970s electronic game, SIMON.</p>
+				</div>
+				<div className='simon-images'>
+					<img classname='simon-title' src={Title} alt='simon'></img>
+					<img classname='simon-mobile' src={Mobile} alt='simon'></img>
+					<img classname='simon-desktop' src={Desktop} alt='simon'></img>
+				</div>
+				<div>
+					<div>
+						<a href='https://caleb-hollingsworth.github.io/SIMON-GAME/'>
+							<AiOutlineLink />
+							deployed application
+						</a>
+						<a href='https://github.com/Caleb-Hollingsworth/SIMON-GAME'>
+							<AiOutlineGithub />
+							GitHub repository
+						</a>
+						<a href=''>
+							<AiOutlineMedium />
+							Blog post coming soon!
+						</a>
+					</div>
 				</div>
 			</main>
 		</div>
