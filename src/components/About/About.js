58dd327684b4c5c37cdab6fd91407b41a55e9e48
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-09-23 12:09:34 -0500

    added styling to contact component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,66 +1,23 @@
 import React, { useState, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import Contact from './Contact/Contact';
-import Resume from './Resume/Resume';
-import Technologies from './Technologies/Technologies';
 import './About.css';
-const scrollToRef = (ref) => window.scrollTo(0, ref.current.scrollIntoView());
 
-const About = () => {
-	const fieldRef = useRef();
-	const executeScroll = () => scrollToRef(fieldRef);
-	const [renderContact, setRenderContact] = useState(false);
-	const [renderResume, setRenderResume] = useState(false);
-	const [renderTechs, setRenderTechs] = useState(false);
-
-	const renderContactHandler = () => {
-		setRenderContact(!renderContact);
-		setRenderResume(false);
-		setRenderTechs(false);
-		executeScroll();
-	};
-	const renderResumeHandler = () => {
-		setRenderResume(!renderResume);
-		setRenderContact(false);
-		setRenderTechs(false);
-	};
-	const renderTechsHandler = () => {
-		setRenderTechs(!renderTechs);
-		setRenderContact(false);
-		setRenderResume(false);
-	};
+const About = (props) => {
 	return (
-		<div className='about-list'>
-			<div className='about-list-buttons'>
-				<button className='about-contact' onClick={renderContactHandler}>
-					contact
-				</button>
-				<button className='about-resume' onClick={renderResumeHandler}>
-					resumé
-				</button>
-				<button className='about-tech' onClick={renderTechsHandler}>
-					technologies
-				</button>
-			</div>
-			<div>
-				{renderContact === true && (
-					<div>
-						{' '}
-						<Contact ref={fieldRef} />
-					</div>
-				)}
-				{renderResume === true && (
-					<div>
-						{' '}
-						<Resume />
-					</div>
-				)}
-				{renderTechs === true && (
-					<div>
-						{' '}
-						<Technologies />
-					</div>
-				)}
+		<div className='about-group'>
+			<div className='about-list'>
+				<div className='about-list-buttons'>
+					<button
+						className='about-contact'
+						onClick={props.renderContactHandler}>
+						contact
+					</button>
+					<button className='about-resume' onClick={props.renderResumeHandler}>
+						resumé
+					</button>
+					<button className='about-tech' onClick={props.renderTechsHandler}>
+						technologies
+					</button>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from '../../components/Home/Home';
+import Contact from '../About/Contact/Contact';
+import Resume from '../About/Resume/Resume';
+import Technologies from '../About/Technologies/Technologies';
 import './Main.css';
 
 const Main = () => {
@@ -8,6 +11,39 @@ const Main = () => {
 	const renderHomeHandler = () => {
 		setRenderHome(!renderHome);
 	};
+	//About section hooks
+	const [renderContact, setRenderContact] = useState(false);
+	const [renderResume, setRenderResume] = useState(false);
+	const [renderTechs, setRenderTechs] = useState(false);
+	const [renderAbout, setRenderAbout] = useState(false);
+	const [renderProjects, setRenderProjects] = useState(false);
+	const renderAboutHandler = () => {
+		setRenderAbout(!renderAbout);
+		setRenderProjects(false);
+	};
+	const renderProjectsHandler = () => {
+		setRenderProjects(!renderProjects);
+		setRenderAbout(false);
+	};
+
+	const renderContactHandler = () => {
+		setRenderContact(!renderContact);
+		setRenderResume(false);
+		setRenderTechs(false);
+	};
+	const renderResumeHandler = () => {
+		setRenderResume(!renderResume);
+		setRenderContact(false);
+		setRenderTechs(false);
+	};
+	const renderTechsHandler = () => {
+		setRenderTechs(!renderTechs);
+		setRenderContact(false);
+		setRenderResume(false);
+	};
+
+	//Projects section hooks
+
 	return (
 		<div className='main-page'>
 			<div className='main-content'>
@@ -16,8 +52,38 @@ const Main = () => {
 					<h2>Software Developer</h2>
 				</button>
 
-				<div>{renderHome === true && <Home />}</div>
+				<div>
+					{renderHome === true && (
+						<Home
+							renderAbout={renderAbout}
+							renderProject={renderProjects}
+							renderAboutHandler={renderAboutHandler}
+							renderProjectsHandler={renderProjectsHandler}
+							renderContactHandler={renderContactHandler}
+							renderResumeHandler={renderResumeHandler}
+							renderTechsHandler={renderTechsHandler}
+						/>
+					)}
+				</div>
 			</div>
+			{renderAbout === true && renderContact === true && (
+				<div className='contact-component'>
+					{' '}
+					<Contact />
+				</div>
+			)}
+			{renderAbout === true && renderResume === true && (
+				<div>
+					{' '}
+					<Resume />
+				</div>
+			)}
+			{renderAbout === true && renderTechs === true && (
+				<div>
+					{' '}
+					<Technologies />
+				</div>
+			)}
 		</div>
 	);
 };
