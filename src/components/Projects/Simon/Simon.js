9740b4cd63d2c065c2f40eae3fad95312f4761e2
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-10-21 12:33:43 -0500

    added corrected styling to all projects

--- src/components/Projects/Simon/Simon.js
+++ src/components/Projects/Simon/Simon.js
@@ -12,27 +12,38 @@ const Simon = () => {
 
 			<main className='simon-content'>
 				<div className='simon-message'>
-					<p>An application using HTML, CSS, & JavaScript</p>
-					<p>A digital version of the 1970s electronic game, SIMON.</p>
+					<h4>An application using HTML, CSS, & JavaScript</h4>
+					<p>
+						This application is a digital version of the 1970s electronic game,
+						SIMON. This is a memory game of 20 levels using buttons and sounds.
+						It includes a counter for the current level, a replay button to
+						replay the last sequence of the current level, a 'Restart Game'
+						button, and a built in function that flashes 'Wrong' and replays the
+						last sequence if the player inputs the sequence incorrectly.
+					</p>
 					<div className='simon-icons'>
 						<div>
 							<a
 								href='https://caleb-hollingsworth.github.io/SIMON-GAME/'
 								rel='noopener noreferrer'
 								target='_blank'>
-								<AiOutlineLink size='lg' />
+								<AiOutlineLink size='2em' />
 							</a>
+							<p>Link to live application</p>
+						</div>
+						<div>
 							<a
 								href='https://github.com/Caleb-Hollingsworth/SIMON-GAME'
 								target='_blank'
 								rel='noopener noreferrer'>
-								<AiOutlineGithub size='lg' />
+								<AiOutlineGithub size='2em' />
 							</a>
-							{/* <div>
+							<p>Link to Github repository</p>
+						</div>
+						{/* <div>
 							<AiOutlineMedium />
 							Blog post coming soon!
 						</div> */}
-						</div>
 					</div>
 				</div>
 				<img className='simon-title' src={Title} alt='simon'></img>
